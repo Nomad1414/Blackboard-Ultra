bd562f7575a0ae2d2fdd0911d7b3b54fb3aaccfa
Nomad1414 <73662336+Nomad1414@users.noreply.github.com>
2022-01-20 10:48:06 +0000

    Update SEUBlackBoard.user.js

--- SEUBlackBoard.user.js
+++ SEUBlackBoard.user.js
@@ -3,38 +3,47 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://lms.seu.edu.sa/*
 // @grant       none
-// @version     1.2
+// @version     1.3
 // @author      Nomad1414
 // @description 02/02/2021, 15:52:38
 // @updateURL   https://github.com/Nomad1414/Blackboard-Ultra/raw/main/SEUBlackBoard.user.js
 // @downloadURL https://github.com/Nomad1414/Blackboard-Ultra/raw/main/SEUBlackBoard.user.js
+// @run-at document-end
 // ==/UserScript==
 
-const course = ["/webapps/collab-ultra/tool/collabultra?course_id=_101924_1", "/webapps/collab-ultra/tool/collabultra?course_id=_101901_1",
+const course = ["Blackboard","/webapps/collab-ultra/tool/collabultra?course_id=_101924_1", "/webapps/collab-ultra/tool/collabultra?course_id=_101901_1",
     "/webapps/collab-ultra/tool/collabultra?course_id=_101839_1","/webapps/collab-ultra/tool/collabultra?course_id=_101940_1"
 ];
-const Names = ["--IT-448--", "--IT-446--", "--IT440--","IT-490"];
+const Names = ["Blackboard Ultra","--IT-448--", "--IT-446--", "--IT440--","--IT-490--"];
 
 try {
-    for (let j = 0; j <= 4; j++) {
+    for (let j = 4; j >= 0; j--) {
         let a = document.createElement("a");
-        a.textContent = Names[j];
-        a.href = course[j];
-        document.getElementsByClassName("collapsible")[0].append(a);
+        var b = document.createElement("li");
+        b.innerHTML = " <a href= " + course[j] +">"+ Names[j] + "</a>";
+        document.getElementById("$fixedId").prepend(b);       
     }
-} catch (error) {
+  }
+
+catch (error) {
     console.error(error);
 }
+
+
 try {
-    for (let i = 4; i >= 0; i--) {
+   for (let i = 4; i >= 0; i--) {
         let li = document.createElement("a");
         li.textContent = Names[i];
         li.href = course[i];
         document.getElementById("courseMenuPalette_contents").prepend(li);
 
     }
-} catch (error) {
+ }
+
+catch (error) {
     console.error(error);
 }
+
+
 //test
 //test
